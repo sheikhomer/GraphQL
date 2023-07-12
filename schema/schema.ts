b51a983499e960266be2298e78adab70c93f83af
Sheikh Omer <ska.omer@yahoo.com>
2023-07-12 11:39:32 +0100

    updated2

--- schema/schema.ts
+++ schema/schema.ts
@@ -1,10 +1,14 @@
-import {
-  GraphQLInt,
-  GraphQLObjectType,
-  GraphQLSchema,
-  GraphQLString,
-} from "graphql";
+import { GraphQLInt, GraphQLObjectType, GraphQLSchema, GraphQLString } from "graphql";
 import axios from "axios";
+
+const CompanyType = new GraphQLObjectType({
+  name: "Company",
+  fields: {
+    id: { type: GraphQLString },
+    name: { type: GraphQLString },
+    description: { type: GraphQLString },
+  },
+});
 
 const UserType = new GraphQLObjectType({
   name: "User",
@@ -12,6 +16,12 @@ const UserType = new GraphQLObjectType({
     id: { type: GraphQLString },
     firstName: { type: GraphQLString },
     age: { type: GraphQLInt },
+    company: {
+      type: CompanyType,
+      resolve(source, args) {
+        return axios.get(`http://localhost:3000/companies/${source.companyId}`).then((resp) => resp.data);
+      },
+    },
   },
 });
 
@@ -22,9 +32,14 @@ const RootQuery = new GraphQLObjectType({
       type: UserType,
       args: { id: { type: GraphQLString } },
       resolve(source, args) {
-        return axios
-          .get(`http://localhost:3000/users/${args.id}`)
-          .then((resp) => resp.data);
+        return axios.get(`http://localhost:3000/users/${args.id}`).then((resp) => resp.data);
+      },
+    },
+    company: {
+      type: CompanyType,
+      args: { id: { type: GraphQLString } },
+      resolve(source, args) {
+        return axios.get(`http://localhost:3000/companies/${args.id}`).then((resp) => resp.data);
       },
     },
   },
